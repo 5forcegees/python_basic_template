543e72e83710b0371d10b0b1610aa430e39913d7
5forcegees <5forcegees@users.noreply.github.com>
2025-04-27 03:56:30 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="5forcegees"
+original_name="python_basic_template"
+original_urlname="python_basic_template"
+original_description="Awesome python_basic_template created by 5forcegees"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv python_basic_template $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ python_basic_template/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for python_basic_template."""
+
+from python_basic_template.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
